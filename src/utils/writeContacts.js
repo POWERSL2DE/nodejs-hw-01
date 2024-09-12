e75bfae424a03930e9874327ff243eafe9663781
POWERSL2DE <naziksl2de@gmail.com>
2024-09-12 20:43:22 +0800

    Summary
    fixed writeContacts.js

--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,10 +1,10 @@
 import { PATH_DB } from '../constants/contacts.js';
 import * as fs from 'node:fs/promises';
-import readContacts from './readContacts.js';
+// import readContacts from './readContacts.js';
 
 
 const writeContacts = async (updatedContacts) => {
-    updatedContacts = JSON.parse(readContacts);
+    // updatedContacts = JSON.parse(readContacts);
 
     await fs.writeFile(PATH_DB, JSON.stringify(updatedContacts, null, 2));
 
