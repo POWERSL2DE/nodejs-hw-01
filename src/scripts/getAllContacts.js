468a61a5937c2e41e3815a2330995ff26f767471
POWERSL2DE <naziksl2de@gmail.com>
2024-09-13 19:22:30 +0800

    Summary

--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -3,16 +3,12 @@ import { PATH_DB } from '../constants/contacts.js';
 
 
 const getAllContacts = async () => {
-    const data = await fs.readFile(PATH_DB, { encoding: 'utf-8'} );
-    return JSON.parse(data);
+  const data = await fs.readFile(PATH_DB, { encoding: 'utf-8'} );
+  return JSON.parse(data);
+
 };
 
-
-getAllContacts()
-  .then((data) => data)
-  .catch((error) => console.error(error));
+export default getAllContacts;
 
 
 console.log(await getAllContacts());
-
-export default getAllContacts;
--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,12 +1,11 @@
-import { PATH_DB } from '../constants/contacts.js';
-import * as fs from 'node:fs/promises';
+// import { PATH_DB } from '../constants/contacts.js';
+// import * as fs from 'node:fs/promises';
 
 
-const readContacts = async () => {
-    const data = await fs.readFile(PATH_DB, 'utf-8');
-    const readData =  JSON.parse(data);
-    readData.shift();
+// const readContacts = async () => {
+//     const data = await fs.readFile(PATH_DB, { encoding: 'utf-8'} );
+//     return JSON.parse(data);
 
-};
+// };
 
-export default readContacts;
+// export default readContacts;
